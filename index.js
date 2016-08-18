32d6a4f029afbdccd7c9a4b43c7d981c68d34c59
Eugene Dobry <edory@gmail.com>
2016-08-18 10:13:25 -0400

    reworked handlers, accept box name and keep count of duplicates

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
 var http = require("http"),
+    qs = require("querystring"),
     path = require("path"),
     fs = require("fs");
 
 var books = {};
 
-var parseData = params => params.split(';')
-    .map(param => param.split('='))
-    .reduce((obj, param) => {
-        obj[param[0]] = param[1];
-        return obj;
-    }, {});
-
 var getData = (req, cb) => {
     var data = "";
 
+    LOG("getting data");
     req.on("data", chunk => data += chunk);
-    req.on("end", () => {
-        //console.log(`data: ${data}`);
 
-        var params = parseData(data);
+    req.on("end", () => {
+        // LOG("parsing");
+        var params = qs.parse(data);
+
+        // LOG("passing to handler");
         cb(params);
     });
 };
 
+var id = x => x;
+var toString = val => val.toString();
+var constant = val => () => val;
+
 var serializers = {
-    "string": val => val.toString(),
-    "boolean": val => val.toString(),
-    "object": obj => JSON.stringify(obj),
-    "undefined": obj => ""
+    string: id,
+    boolean: toString,
+    object: obj => JSON.stringify(obj),
+    date: date => date.toISOString(),
+    undefined: constant("")
 };
 
 var serialize = val => serializers[typeof val](val);
 
-var writeReturn = (res, func) => (...args) => res.end(serialize(func(...args)));
+var writeReturn = (res, func) => (...args) => {
+    var returnVal = serialize(func(...args));
+
+    // LOG(`returned: ${returnVal}`);
+    res.end(returnVal);
+};
 
 var dbFilePath = path.join(__dirname, "books.json");
 
@@ -41,21 +48,29 @@ var LOG = message => console.log(message);
 var createDbFile = data => {
     LOG(`creating db file at ${dbFilePath}`);
 
-    console.log("data", data);
-    fs.writeFile(dbFilePath, JSON.stringify(data), err => {
+    // LOG(`data ${Object.keys(data)} ${serialize(data)}`);
+    fs.writeFile(dbFilePath, serialize(data), err => {
         if(err) {
             console.log("error:" + err);
             throw new Error(`could not create db file: ${err.message}`);
         }
 
-        LOG(`db file created at ${dbFilePath}`);
+        LOG("db file created");
     });
 };
 
 var toPairs = obj => Object.keys(obj).map(key => [key, obj[key]]);
 
 var merge = a => ({
-    into: b => toPairs(a).forEach(pair => b[pair[0]] = pair[1])
+    into: b => toPairs(a).forEach(pair => {
+        var bVal = b[pair[0]];
+        if(bVal && !!bVal.count) {
+            LOG(`incremented count to ${++bVal.count}`)
+            return;
+        }
+
+        b[pair[0]] = pair[1];
+    })
 });
 
 var syncBooks = cb => {
@@ -63,6 +78,8 @@ var syncBooks = cb => {
         if(err) {
             LOG("db file not found");
             createDbFile(books);
+
+            cb(books);
         }
 
         var loadedBooks;
@@ -74,7 +91,6 @@ var syncBooks = cb => {
 
         LOG("merging dbs");
         merge(books).into(loadedBooks);
-
         fs.writeFile(dbFilePath, serialize(loadedBooks), err => {
             if(err)
                 throw new Error(err.message);
@@ -83,40 +99,52 @@ var syncBooks = cb => {
             LOG("db synchronized");
             cb(loadedBooks);
         });
-    })
+    });
 };
 
-var addBook = ({code}) => {
-    books[code] = { id: code };
+var addBook = ({code, box}) => {
+    books[code] = {
+        id: code,
+        box: box,
+        added: new Date(),
+        count: 0
+    };
 
-    syncBooks(() => LOG(`added book: ${code}`));
+    syncBooks(() => LOG(`added book ${code} to box: ${box}`));
 };
 
 var listBooks = () => {
-    console.log("listing books");
+    LOG("listing books");
     return books;
 };
 
-var routes = {
-    "POST /book": (req, res) => getData(req, writeReturn(res, addBook)),
-    "GET /book": (req, res) => writeReturn(res, listBooks)()
+var methodWrappers = {
+    POST: (req, res) => handler => getData(req, writeReturn(res, handler)),
+    GET: (req, res) => handler => writeReturn(res, handler)()
 };
 
 var hashRequest = req => `${req.method} ${req.url}`;
 
-var server = http.createServer((req, res) => {
+var handleReq = (req, res) => {
     var reqKey = hashRequest(req);
-
-    console.log(`request: ${reqKey}`);
+    LOG(`request: ${reqKey}`);
 
     var handler = routes[reqKey];
     if(!handler) {
+        res.setHead(404);
         res.end();
         return;
     }
 
-    handler(req, res);
-});
+    methodWrappers[req.method](req, res)(handler);
+}
+
+var routes = {
+    "POST /book": addBook,
+    "GET /book": listBooks
+};
+
+var server = http.createServer(handleReq);
 
 var init = () => {
     syncBooks(books => {
